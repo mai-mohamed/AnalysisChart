3bb95b0dac99eea66d15885df809360bcb8e49e5
mai-mohamed <maimohamedag@gmail.com>
2022-04-01 23:24:17 +0200

    adding interceptor

---
+++ src/config/interceptor/interceptor.tsx
@@ -0,0 +1,42 @@
+import axios, {
+    AxiosError,
+    AxiosInstance,
+    AxiosRequestConfig,
+    AxiosResponse,
+  } from "axios";
+  
+  const onRequest = (config: any): AxiosRequestConfig => {  
+    console.info(`[request] [${JSON.stringify(config)}]`);
+    return config;
+  };
+  
+  const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[request error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+  
+  const onResponse = (response: AxiosResponse): AxiosResponse => {
+    console.info(`[response] [${JSON.stringify(response)}]`);
+    return response;
+  };
+  
+  const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[response error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+  
+  export function setupInterceptorsTo(
+    axiosInstance: AxiosInstance
+  ): AxiosInstance {
+    axiosInstance.interceptors.request.use(onRequest, onRequestError);
+    axiosInstance.interceptors.response.use(onResponse, onResponseError);
+    return axiosInstance;
+  }
+  
+  export const axiosInstance = axios.create({
+    baseURL: "http://localhost:4000",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  
