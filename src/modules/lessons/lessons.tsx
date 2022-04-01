3e6aaa35a361994b7a388d83a13d5b1eedd245df
mai-mohamed <maimohamedag@gmail.com>
2022-04-02 00:42:10 +0200

    retrieving data to main lessons componant

--- src/modules/lessons/lessons.tsx
+++ src/modules/lessons/lessons.tsx
@@ -1,9 +1,12 @@
 import {FC, useEffect} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {ILessons} from "../../shared/models/lessons.model";
 import {getLessonsData} from "./redux/actions";
 
 const Lessons:FC=()=>{
     const dispatch=useDispatch();
+    const lessons =useSelector((state:ILessons)=>state.lessons)
+    console.log(lessons)
     useEffect(()=>{
         dispatch(getLessonsData())
     },[])
