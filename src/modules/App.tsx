a05af34527822aa9bcf5e550c8c2ba9346532c95
mai-mohamed <maimohamedag@gmail.com>
2022-04-02 20:42:50 +0200

    adding conditional redering for loader

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,24 +1,34 @@
-import {FC, useState} from "react";
-import {  useSelector } from "react-redux";
+import { FC, useState } from "react";
+import { useSelector } from "react-redux";
 import Lessons from "./lessons/lessons";
 import DarkMode from "./darkMode/darkMode";
-import "./../assets/scss/main.scss"
+import "./../assets/scss/main.scss";
+import Loader from "../shared/components/loader/loader";
 
-const App:FC=()=>{
-    const [isDarkMode,setDarkMode]=useState<boolean>(false)
-    const isLoading =useSelector((state:any)=>state.loader.loading)
-    
-    const handleDarkModeChange=()=>{
-        setDarkMode(!isDarkMode)
-    }
-    return(    
-        <div className={isDarkMode?"dark-mode":"light-mode"}>
-            <div className="container">
-            <DarkMode handleDarkModeChange={handleDarkModeChange} isDark={isDarkMode}/>
-            <Lessons/>
-            </div>
+const App: FC = () => {
+  const [isDarkMode, setDarkMode] = useState<boolean>(false);
+  const isLoading = useSelector((state: any) => state.loader.loading);
+
+  const handleDarkModeChange = () => {
+    setDarkMode(!isDarkMode);
+  };
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+          <div className="container">
+            <DarkMode
+              handleDarkModeChange={handleDarkModeChange}
+              isDark={isDarkMode}
+            />
+            <Lessons />
+          </div>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
 export default App;
