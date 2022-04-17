402e1ef1b1fa3acde3edb7dd4317c6d0776906b4
mai-mohamed <maimohamedag@gmail.com>
2022-04-17 04:16:35 +0200

    adding main react router file

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./config/redux";
-import App from "./modules/App";
+import { BrowserRouter } from "react-router-dom";
+import RouterComponent from "./config/router/Route";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <RouterComponent />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
---
+++ src/modules/lessonDetails/lessonDetails.tsx
@@ -0,0 +1,10 @@
+
+
+const LessonDetails=()=>{
+    return(
+        <div>
+            dtails
+        </div>
+    )
+}
+export default LessonDetails
