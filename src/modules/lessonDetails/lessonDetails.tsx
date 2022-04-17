c6cc4d25b1926a633ed4f1b14dd12405f65765c0
mai-mohamed <maimohamedag@gmail.com>
2022-04-17 05:10:08 +0200

    retreivng lesson details

--- src/modules/lessonDetails/lessonDetails.tsx
+++ src/modules/lessonDetails/lessonDetails.tsx
@@ -1,10 +1,12 @@
+import { getURLParams } from "../../shared/utils/utils";
 
-
-const LessonDetails=()=>{
-    return(
-        <div>
-            dtails
-        </div>
-    )
-}
-export default LessonDetails
+const LessonDetails = () => {
+  return (
+    <div>
+      <p>School: {getURLParams("school")}</p>
+      <p>Camp: {getURLParams("camp")}</p>
+      <p>Country: {getURLParams("country")}</p>
+    </div>
+  );
+};
+export default LessonDetails;
