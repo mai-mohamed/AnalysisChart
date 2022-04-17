c2781a8037e2548a4be9b3a9fb913898603fae4f
mai-mohamed <maimohamedag@gmail.com>
2022-04-17 02:51:20 +0200

    handling select all schools

--- src/modules/lessons/components/allSchoolsSection/allSchoolsSection.tsx
+++ src/modules/lessons/components/allSchoolsSection/allSchoolsSection.tsx
@@ -1,13 +1,37 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectOptions } from "../../../../shared/models/selectOptions.model";
 
-const AllSchoolsSection: FC = () => {
+const AllSchoolsSection = ({ handleCheckBoxChange }: any) => {
   const schools = useSelector(({ lessons }: any) => lessons.schoolsData);
+  const [selectedSchools, setSelectedSchools] = useState<string[]>([]);
+  const handleCheckboxChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    school: any
+  ) => {
+    if (e.target.checked) {
+      setSelectedSchools([...selectedSchools, school.label]);
+      handleCheckBoxChange([...selectedSchools, school.label]);
+    } else {
+      const filteredArr = selectedSchools.filter(
+        (item) => item !== school.label
+      );
+      setSelectedSchools(filteredArr);
+      handleCheckBoxChange(filteredArr);
+    }
+  };
   return (
     <div className="all-schools-wrapper">
       {schools.map((school: selectOptions, i: number) => (
-        <div key={i}>{school.label}</div>
+        <div key={i}>
+          <input
+            type={"checkbox"}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              handleCheckboxChange(e, school)
+            }
+          />
+          <label htmlFor={school.label}>{school.label}</label>
+        </div>
       ))}
     </div>
   );
--- src/modules/lessons/components/charts/charts.tsx
+++ src/modules/lessons/components/charts/charts.tsx
@@ -25,13 +25,21 @@ ChartJS.register(
 
 type IProps = {
   data: ILessons[];
-  school?: string;
+  school?: string | string[];
   camp?: string;
   country?: string;
 };
 
+type IDataSet = {
+  label: string;
+  data: [];
+  fill: boolean;
+  backgroundColor: string;
+  borderColor: string;
+};
 const Charts: FC<IProps> = (props: IProps) => {
   const [lessons, setLessons] = useState<any>();
+  const [dataSets, setDataSets] = useState<IDataSet[]>([]);
   useEffect(() => {
     const yAxis = props.data.filter((row) => {
       return (
@@ -45,23 +53,68 @@ const Charts: FC<IProps> = (props: IProps) => {
       return row.lessons;
     });
     setLessons(lessonsArr);
-  }, [props.data, props.school, props.camp,props.country]);
+    getDataSets(lessonsArr);
+  }, [props.data, props.school, props.camp, props.country]);
+  
+  const getDataSets = (lessonsArr:any) => {
+    if (typeof props.school == "string") {
+      setDataSets([
+        {
+          label: props.school,
+          data: lessonsArr,
+          fill: false,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        },
+      ]);
+    } else {
+      //@ts-ignore
+      if (props.school?.length > 0) {
+        const getLessons = (school: string) => {
+          const yAxis = props.data.filter((row) => {
+            return (
+              row.school == school &&
+              row.camp == props.camp &&
+              row.country == props.country
+            );
+          });
 
+          const lessonsArr = yAxis.map((row) => {
+            return row.lessons;
+          });
+          return lessonsArr;
+        };
+        //@ts-ignore
+        const dataSetArr = props.school.map(((item,i) => ({
+          label: item,
+          data: getLessons(item),
+          fill: false,
+          // backgroundColor: "rgba(75,192,192,0.2)",
+          // borderColor: "rgba(75,192,192,1)",
+           backgroundColor: `rgba(${Math.floor(Math.random() * 200) + 26},${
+            Math.floor(Math.random() * 200) + 26
+          },${Math.floor(Math.random() * 200) + 26},0.2)`,
+          
+           borderColor: `rgba(${Math.floor(Math.random() * 200) + 26},${
+            Math.floor(Math.random() * 200) + 26
+          },${Math.floor(Math.random() * 200) + 26},.6)`,
+          
+        })))
+        //@ts-ignore
+        setDataSets(dataSetArr);
+      }
+    }
+  };
   const chartData = {
     labels: monthsArr,
-    datasets: [
-      {
-        label: props.school,
-        data: lessons,
-        fill: false,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)",
-      },
-    ],
+    datasets: dataSets,
   };
   return (
     <div>
-      <Line data={chartData} />
+      <Line
+        //@ts-ignore
+        data={chartData}
+      />
     </div>
   );
 };
